6c65985c3c66265c85e5106dcee3f4f061dc4d44
kazuya.zama <zama@restarts.co.jp>
2023-01-12 13:03:42 +0900

    title修正

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,12 +1,12 @@
 export default {
-    titleTemplate: '%s | 株式会社リス・ターツ',
-    defaultTitle: '株式会社リス・ターツ',
+    titleTemplate: '%s | 株式会社リ・スターツ',
+    defaultTitle: '株式会社リ・スターツ',
     description:"株式会社リ・スターツの公式コーポレートサイトです。About,News、Service,Company等掲載しています",
     openGraph: {
       url: 'https://www.restarts.co.jp/',
       type: 'website',
       locale: 'ja_JP',
-      site_name: '株式会社リス・ターツ',
+      site_name: '株式会社リ・スターツ',
       images: [
         {
           url: "",
