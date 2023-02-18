190965d173c038eeebfb8ba3fbdd601f93d41198
kazuya.zama <zama@restarts.co.jp>
2023-02-18 20:03:54 +0900

    xportPathMapを追加

--- next.config.js
+++ next.config.js
@@ -7,6 +7,18 @@ const nextConfig = {
     disableStaticImages: true, // importした画像の型定義設定を無効にする
     unoptimized: true,
   },
+  trailingSlash: true, // スラッシュ強制
+  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' }, // スラッシュ強制したいのでpathにもスラッシュ入れる
+      '/company': { page: '/company' }, // スラッシュ強制したいのでpathにもスラッシュ入れる
+      '/partner': { page: '/partner' }, // スラッシュ強制したいのでpathにもスラッシュ入れる
+      '/service': { page: '/service' }, // スラッシュ強制したいのでpathにもスラッシュ入れる
+      '/privacy': { page: '/privacy' }, // スラッシュ強制したいのでpathにもスラッシュ入れる
+      '/contact': { page: '/contact' }, // スラッシュ強制したいのでpathにもスラッシュ入れる
+    }
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
